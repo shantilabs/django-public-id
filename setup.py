e52abe2b11405daa6a6cde47c8d60cd36a942efb
kidig <kiddima@gmail.com>
2017-06-08 00:00:14 +0300

    update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,38 @@
-from distutils.core import setup
+#!/usr/bin/env python
+import os
+import sys
+from setuptools import setup, find_packages
+
+version = __import__('public_id').__version__
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    os.system('python setup.py bdist_wheel upload')
+    sys.exit()
+
 
 
 setup(
     name='django-public-id',
-    version='1.1',
+    version=version,
     author='Maxim Oransky',
     author_email='maxim.oransky@gmail.com',
+    maintainer='Dmitrii Gerasimenko',
+    maintainer_email='kiddima@gmail.com',
     url='https://github.com/shantilabs/django-public-id',
-    packages=[
-        'public_id'
+    packages=find_packages(exclude=["test_django"]),
+    zip_safe=False,
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
     ],
-    #package_data={
-    #    'public_id': ['locale/ru/LC_MESSAGES/*'],
-    #},
 )
