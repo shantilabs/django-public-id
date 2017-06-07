34210127a8ab367aca255fc9e716686ba7655aea
kidig <kiddima@gmail.com>
2017-06-08 00:33:56 +0300

    fix test for django 1.8

--- test_django/settings.py
+++ test_django/settings.py
@@ -1,5 +1,7 @@
 import os
 import string
+from distutils.version import LooseVersion
+import django
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -116,6 +118,9 @@ STATIC_URL = '/static/'
 PUBLIC_ID_CHARS = string.digits + string.ascii_lowercase + string.ascii_uppercase
 
 
-MIGRATION_MODULES = {
-    'testapp': None,
-}
+DJANGO_1_9 = LooseVersion(django.get_version()) > LooseVersion('1.9')
+
+if DJANGO_1_9:
+    MIGRATION_MODULES = {
+        'testapp': None,
+    }
